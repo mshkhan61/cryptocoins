c34362ca43b6ba5847421af679f3eb596a01d0f4
Howard Chung <Howard@new-host-2.home>
2013-08-13 22:03:38 -0700

    fixed adaptation errors

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,22 +1,12 @@
 class HomeController < ApplicationController
 	require 'open-uri'
+	require 'json'
 
 def index
-	data={}
-	map={}
-	cycle=2016
-	map["price"] = 1
-	map["block"] = open("http://blockexplorer.com/q/getblockcount").read.to_i
-	map["nextretarget"] = open("http://blockexplorer.com/q/nextretarget").read.to_i
-	map["blocksleft"]=map["nextretarget"]-map["block"]
-	map["measured"]=(cycle-map["blocksleft"])/float(cycle)
-	map["difficulty"]=open("http://blockexplorer.com/q/getdifficulty").read.to_i
-	map["estdifficulty"]=open("http://blockexplorer.com/q/estimate").read.to_i
-	map["timeretarget"]=open("http://blockexplorer.com/q/eta").read.to_i
-	data["btc"]=map
-
-	generateMapData("ltc","http://litecoinscout.com/chain/litecoin/q",150,2016)
-	generateMapData("ftc","http://explorer.feathercoin.com/chain/Feathercoin/q",150,504)
+	@data={}
+	@data["btc"]=generateBTCData(2016)
+	@data["ltc"]=generateMapData("ltc","http://litecoinscout.com/chain/litecoin/q",150,2016)
+	@data["ftc"]=generateMapData("ftc","http://explorer.feathercoin.com/chain/Feathercoin/q",150,504)
 end
 
 def generateMapData(name, url, blocktime, cycle)
@@ -30,16 +20,29 @@ def generateMapData(name, url, blocktime, cycle)
 	fullcycletime=blocktime*cycle
 	lastretarget = (map["block"]/cycle)*cycle
 	cycleblocks=map["block"]-lastretarget
-	lastcycletime=open(url+"/nethash/1/"+str(cycleblocks*-1)+"/"+str(cycleblocks*-1+1)).read.split("START DATA")[1].split(",")[1].to_i
-	currcycletime=int(nethash[1])-lastcycletime
+	lastcycletime=open(url+"/nethash/1/"+(cycleblocks*-1).to_s+"/"+(cycleblocks*-1+1).to_s).read.split("START DATA")[1].split(",")[1].to_i
+	currcycletime=nethash[1].to_i-lastcycletime
 
 	map["nextretarget"] = lastretarget+cycle
 	map["blocksleft"]=map["nextretarget"]-map["block"]
-	map["measured"]=cycleblocks/float(cycle)
-	map["difficulty"]=float(nethash[4])
+	map["measured"]=cycleblocks/cycle.to_f
+	map["difficulty"]=nethash[4].to_f
 	map["estdifficulty"]=fullcycletime/(currcycletime/map["measured"])*map["difficulty"]
 	map["timeretarget"]=map["blocksleft"]*hashesperblock/nethashrate
-	data[name]=map
+	return map
+end
+
+def generateBTCData(cycle)
+	map={}
+	map["price"] = 1
+	map["block"] = open("http://blockexplorer.com/q/getblockcount").read.to_i
+	map["nextretarget"] = open("http://blockexplorer.com/q/nextretarget").read.to_i
+	map["blocksleft"]=map["nextretarget"]-map["block"]
+	map["measured"]=(cycle-map["blocksleft"])/cycle.to_f
+	map["difficulty"]=open("http://blockexplorer.com/q/getdifficulty").read.to_i
+	map["estdifficulty"]=open("http://blockexplorer.com/q/estimate").read.to_i
+	map["timeretarget"]=open("http://blockexplorer.com/q/eta").read.to_i
+	return map
 end
 
 end
